e1fa77dc99f92a533530f0344b1e5280cb458f6c
晨辉 <cheney_zhang@foxmail.com>
2022-12-10 23:38:49 +0800

    手写节流防抖

---
+++ js/手写节流防抖.js
@@ -0,0 +1,71 @@
+// 手写节流
+{
+
+    const blink = () => {
+        console.log('闪现')
+    }
+
+    let coolDownFlag = false
+    let timer = null
+
+    if (coolDownFlag === true) return
+    coolDownFlag = true
+    blink()
+
+    timer = setTimeout(() => {
+        coolDownFlag = false
+    }, 10 * 10000)
+
+    const throttle1 = (fn, timeout, asThis) => {
+        let coolDownFlag = false
+        let timer = null
+        return (...args) => {
+            if (coolDownFlag) return
+            coolDownFlag = true
+            fn.call(asThis, ...args)
+            timer = setTimeout(() => {
+                coolDownFlag = false
+            }, timeout)
+        }
+    }
+
+    const throttle2 = (fn, timeout, asThis) => {
+        let timer = null
+        return (...args) => {
+            if (timer) return
+            fn.call(asThis, ...args)
+            timer = setTimeout(() => {
+                timer = null
+            }, timeout)
+        }
+    }
+}
+
+// 手写防抖 回程被打断之后重新回程
+{
+    const f = () => {
+        console.log('开始回程')
+    }
+    let time = null
+    const x = (fn, timeout) => {
+        if (time) {
+            clearTimeout(timer)
+        }
+        timer = setTimeout(() => {
+            fn()
+            timer = null
+        }, timeout)
+    }
+
+    const debounce = (fn, timeout, asThis) => {
+        let timer = null
+        return (...args) => {
+            if(timer) clearTimeout(timer)
+            timer = setTimeout(() => {
+                fn.call(asThis, ...args)
+                timer = null
+            }, timeout)
+        }
+    }
+    const tp = debounce(f)
+}
