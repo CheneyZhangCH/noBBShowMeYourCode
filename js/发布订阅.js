f9c0d907c67807942efed67e8b3e1fd888ad58f0
晨辉 <cheney_zhang@foxmail.com>
2022-12-11 22:14:34 +0800

    手写发布订阅

---
+++ js/发布订阅.js
@@ -0,0 +1,60 @@
+// 手写发布订阅
+{
+    const eventHub = {
+        queueMap: {},
+        on: (name, fn) => {
+            // 添加到队列
+            if (eventHub.queueMap[name] === undefined) eventHub.queueMap[name] = []
+            eventHub.queueMap[name].push(fn)
+        },
+        emit: (name, params) => {
+            const queue = eventHub.queueMap[name]
+            if (!queue) return
+            queue.forEach(fn => {
+                fn.call(undefined, params)
+            })
+        }, // trigger
+        off: (name, fn) => {
+            const queue = eventHub.queueMap[name]
+            if (!queue) return
+            const fnIndex = queue.indexOf(fn)
+            if (fnIndex < 0) return
+            queue.splice(fnIndex, 1)
+        },
+        once: (name, fn) => {
+
+
+        }
+    }
+    eventHub.on('click', console.log)
+    eventHub.on('click', console.error)
+    setTimeout(() => {
+        eventHub.emit('click', '你好')
+    }, 3000)
+}
+
+{
+    class EventHub {
+        map = {}
+        on(name, fn) {
+            this.map[name] = this.map[name] || []
+            this.map[name].push(fn)
+        }
+        emit(name, data) {
+            const fnList = this.map[name] || []
+            fnList.forEach(fn => fn.call(undefined, data))
+        }
+        off(name, fn) {
+            const fnList = this.map[name] || []
+            const index = fnList.indexOf(fn)
+            if (index < 0) return
+            fnList.splice(index, 1)
+        }
+    }
+    // 使用 
+    const e = new EventHub()
+    e.on('click', (name) => { console.log('hi ' + name) })
+    e.on('click', (name) => { console.log('hello ' + name) })
+    setTimeout(() => { e.emit('click', 'frank') }, 3000)
+}
+
